a18296e95e483047828d533a5035d9e00de78ed7
Heri Agape <13354804+0x6d6861@users.noreply.github.com>
2024-06-17 21:05:16 +0300

    fixes

--- frontend/src/pages/protected/layout.tsx
+++ frontend/src/pages/protected/layout.tsx
@@ -1,9 +1,11 @@
 import Topbar from "@/components/Topbar";
 import { Outlet } from "react-router-dom";
+// import { GeistMono } from "geist/font/mono";
+// import { GeistSans } from "geist/font/sans";
 
 function layout() {
   return (
-    <div className="relative flex min-h-screen flex-col bg-gray-50">
+    <div className={`relative flex min-h-screen flex-col bg-gray-50`}>
       <Topbar />
       <div className="container mx-auto flex items-center justify-between px-4 md:px-6">
         <Outlet />
--- frontend/src/pages/public/layout.tsx
+++ frontend/src/pages/public/layout.tsx
@@ -1,9 +1,11 @@
 import Topbar from "@/components/Topbar";
 import { Outlet } from "react-router-dom";
+// import { GeistMono } from "geist/font/mono";
+// import { GeistSans } from "geist/font/sans";
 
 function layout() {
   return (
-    <div className="relative flex min-h-screen flex-col bg-background">
+    <div className={`relative flex min-h-screen flex-col bg-background`}>
       <Topbar />
       <div className="container mx-auto flex items-center justify-between px-4 md:px-6">
         <Outlet />
