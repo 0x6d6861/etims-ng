7b86eb6ea91f467fb228b0874e73077ed7ecd433
Heri Agape <agape@live.fr>
2024-06-21 21:58:34 +0300

    ui updates

--- frontend/src/components/export/ExportModal.tsx
+++ frontend/src/components/export/ExportModal.tsx
@@ -3,54 +3,92 @@ import {
   Dialog,
   DialogContent,
   DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "../ui/dialog";
 import { Button } from "../ui/button";
-import { Label } from "../ui/label";
-import { Input } from "../ui/input";
+import {
+  Step,
+  type StepItem,
+  Stepper,
+  // type StepperProps,
+  useStepper,
+} from "@/components/stepper";
+
+const steps = [
+  { label: "Step 1" },
+  { label: "Step 2" },
+  { label: "Step 3" },
+] satisfies StepItem[];
 
 function ExportModal(props: { children: React.ReactNode }) {
   return (
     <Dialog>
       <DialogTrigger asChild>{props.children}</DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
+      <DialogContent className="sm:max-w-[700px]">
         <DialogHeader>
           <DialogTitle>Edit profile</DialogTitle>
           <DialogDescription>
             Make changes to your profile here. Click save when you're done.
           </DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input
-              id="name"
-              defaultValue="Pedro Duarte"
-              className="col-span-3"
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input
-              id="username"
-              defaultValue="@peduarte"
-              className="col-span-3"
-            />
-          </div>
+        <div className="">
+          <Stepper variant={"line"} initialStep={0} steps={steps}>
+            {steps.map((stepProps, index) => {
+              return (
+                <Step key={stepProps.label} {...stepProps}>
+                  <div className="h-40 flex items-center justify-center my-2 border bg-secondary text-primary rounded-md">
+                    <h1 className="text-xl">Step {index + 1}</h1>
+                  </div>
+                </Step>
+              );
+            })}
+            <Footer />
+          </Stepper>
         </div>
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
 }
 
 export default ExportModal;
+
+const Footer = () => {
+  const {
+    nextStep,
+    prevStep,
+    resetSteps,
+    isDisabledStep,
+    hasCompletedAllSteps,
+    isLastStep,
+    isOptionalStep,
+  } = useStepper();
+  return (
+    <>
+      {hasCompletedAllSteps && (
+        <div className="h-40 flex items-center justify-center my-2 border bg-secondary text-primary rounded-md">
+          <h1 className="text-xl">Woohoo! All steps completed! 🎉</h1>
+        </div>
+      )}
+      <div className="w-full flex justify-end gap-2 mt-8">
+        {hasCompletedAllSteps ? (
+          <Button onClick={resetSteps}>Reset</Button>
+        ) : (
+          <>
+            <Button
+              disabled={isDisabledStep}
+              onClick={prevStep}
+              variant="secondary"
+            >
+              Prev
+            </Button>
+            <Button onClick={nextStep}>
+              {isLastStep ? "Finish" : isOptionalStep ? "Skip" : "Next"}
+            </Button>
+          </>
+        )}
+      </div>
+    </>
+  );
+};
