d448efcbdfb6a3371e122110f7b8998a6d784473
Heri Agape <13354804+0x6d6861@users.noreply.github.com>
2024-07-01 11:20:45 +0300

    email form done

--- frontend/src/components/export/SendEmailModal.tsx
+++ frontend/src/components/export/SendEmailModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Dialog,
   DialogContent,
@@ -8,48 +8,118 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "../ui/dialog";
+import { invoiceData } from "@/components/export/elements/type";
 import { Button } from "../ui/button";
 import { Textarea } from "../ui/textarea";
 import { Input } from "../ui/input";
 import { Separator } from "../ui/separator";
+import { useSelector } from "react-redux";
+import { getUser } from "@/store/features/auth/slice";
+import { User } from "firebase/auth";
 import { Label } from "../ui/label";
+import { Tag, TagInput } from "emblor";
 
 function SendEmailModal(props: { children: React.ReactNode }) {
+  const user: User = useSelector(getUser);
+  const [email, setEmail] = useState<string>();
+  const [subject, setSubject] = useState<string>();
+  const [constent, setContent] = useState<string>();
+
+  const [emails, setEmails] = useState<Tag[]>([]);
+  const [activeTagIndex, setActiveTagIndex] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (user) {
+      setEmails([
+        {
+          id: String(Date.now()),
+          text: user.email as string,
+        },
+      ]);
+      setSubject(
+        `Invoice #${invoiceData.scuInfo["Invoice Number"]} from ${invoiceData.customerDetails.name}`
+      );
+      setContent(`Hi ${user.displayName?.split(" ")[0]}!\n
+This is your invoice for ${
+        invoiceData.scuInfo["Invoice Number"]
+      } in Quickbooks format.\n
+Please find the attached invoice.\n
+Thank you,\n
+eTims Team`);
+    }
+  }, [user]);
+
   return (
     <Dialog>
       <DialogTrigger asChild>{props.children}</DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
+      <DialogContent className="max-w-2xl px-0">
+        <DialogHeader className="px-6">
+          <DialogTitle>Send via email</DialogTitle>
           <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+            This will send an email to {email} with the attached invoice.
           </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4">
-          <div className="space-y-2">
-            <div className="grid w-full items-center gap-1.5">
-              {/* <Label htmlFor="email">Email</Label> */}
-              <Input
-                className="border-none shadow-none text-lg focus-visible:ring-0 px-0"
-                type="email"
-                id="email"
-                placeholder="Email"
+          <div className="space-y-2 px-6">
+            <div className="flex flex-row w-full items-center gap-1.5">
+              <Label
+                className="bg-gray-100 border-b-2 px-3 py-1 rounded-lg"
+                htmlFor="email"
+              >
+                Email
+              </Label>
+
+              <TagInput
+                tags={emails}
+                setTags={(newTags) => {
+                  setEmails(newTags);
+                }}
+                placeholder="Add recepient"
+                styleClasses={{
+                  inlineTagsContainer:
+                    "border-none shadow-none text-lg focus-visible:ring-0 px-0",
+                  input: "border-none shadow-none focus-visible:ring-0",
+                  tag: {
+                    body: "flex items-center bg-transparent font-semibold",
+                    closeButton: "text-red-500 hover:text-red-600",
+                  },
+                }}
+                activeTagIndex={activeTagIndex}
+                setActiveTagIndex={setActiveTagIndex}
+                variant={"default"}
+                shape={"pill"}
+                // borderStyle={"none"}
               />
             </div>
-            <Separator className="h1 border-gray-200" />
-            <Input
-              className="border-none shadow-none text-lg focus-visible:ring-0 px-0"
-              placeholder="Subject"
-            />
-            <Separator className="h1 border-gray-200" />
+            <div className="flex flex-row w-full items-center gap-1.5">
+              <Label
+                className="bg-gray-100 border-b-2 px-3 py-1 rounded-lg"
+                htmlFor="subject"
+              >
+                Subject
+              </Label>
+
+              <Input
+                onChange={(ev) => setSubject(ev.target.value)}
+                value={subject}
+                id="subject"
+                className="border-none shadow-none text-md font-semibold focus-visible:ring-0 px-0"
+                placeholder="Subject"
+              />
+            </div>
+          </div>
+          <Separator className="border-gray-100" />
+
+          <div className="px-6">
             <Textarea
-              rows={8}
-              className="border-none shadow-none text-lg focus-visible:ring-0 px-0"
+              onChange={(ev) => setContent(ev.target.value)}
+              value={constent}
+              rows={9}
+              className="text-md border-none shadow-none focus-visible:ring-0 px-0"
               placeholder="Type your message here."
             />
           </div>
-          <div className="space-y-2">
+          <div className="space-y-2 px-6">
             <div className="flex flex-row items-center gap-2">
               <div className="">
                 <img src="/logos/csv_icon.svg" className="h-8" />
@@ -59,7 +129,7 @@ function SendEmailModal(props: { children: React.ReactNode }) {
             </div>
           </div>
         </div>
-        <DialogFooter className="">
+        <DialogFooter className="px-6">
           <Button type="submit">Send Mail</Button>
         </DialogFooter>
       </DialogContent>
--- frontend/src/pages/protected/etims/single.tsx
+++ frontend/src/pages/protected/etims/single.tsx
@@ -22,70 +22,7 @@ import { Separator } from "@radix-ui/react-separator";
 import { Button } from "@/components/ui/button";
 import NewExportModal from "@/components/export/NewExportModal";
 import { FileUp } from "lucide-react";
-
-const invoiceData = {
-  customerDetails: {
-    name: "PURITY WANJIRU GICHERE",
-    pin: "A002621902D",
-    invoiceNumber: "KRASRN000079638/3",
-  },
-  items: [
-    {
-      clientPin: "P051993765Z",
-      clientName: "Powwater Limited",
-      description: "Supply of natural water",
-      code: "KE1NTXU0000011",
-      unitPrice: 4000,
-      quantity: 15,
-      total: 60000,
-      tax: "D",
-    },
-    {
-      clientPin: "P051993765Z",
-      clientName: "Powwater Limited",
-      description: "Supply of natural water",
-      code: "KE1NTXU0000011",
-      unitPrice: 3000,
-      quantity: 6,
-      total: 18000,
-      tax: "D",
-    },
-    {
-      clientPin: "P051993765Z",
-      clientName: "Powwater Limited",
-      description: "Supply of natural water",
-      code: "KE1NTXU0000011",
-      unitPrice: 4300,
-      quantity: 1,
-      total: 4300,
-      tax: "D",
-    },
-    {
-      clientPin: "P051993765Z",
-      clientName: "Powwater Limited",
-      description: "Supply of natural water",
-      code: "KE1NTXU0000011",
-      unitPrice: 4500,
-      quantity: 48,
-      total: 216000,
-      tax: "D",
-    },
-  ],
-  totals: {
-    TOTAL: 298300,
-    "TOTAL AMOUNT D-Non-VAT": 298300,
-    CASH: 298300,
-    "ITEM NUMBER": 4,
-    "Receipt Number": 3,
-  },
-  scuInfo: {
-    Date: "28/05/2024 00:00:00",
-    "Control Unit Number": "KRASRN000079638",
-    "Invoice Number": "KRASRN000079638/3",
-    "Internal Data": "ETZH-KT3Q-4HDM-COO4-RU46-YPLG-S4",
-    "Receipt Singnature": "DA6K-Q5IF-X5WP-JBIE",
-  },
-};
+import { invoiceData } from "@/components/export/elements/type";
 
 const generatePDF = () => {
   const doc = new jsPDF();
